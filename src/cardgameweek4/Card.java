cc282a10a2cf5f5ea9f09775c1ea96655fe2fe62
Max Matthews <mattmadi@shernet.sheridancollege.ca>
2023-02-24 15:32:55 -0500

    First commit

---
+++ src/cardgameweek4/Card.java
@@ -0,0 +1,34 @@
+package cardgameweek4;
+
+/**
+ * Starter code for the Card class. To be used in Week 4.
+ *
+ * @author dancye, 2019
+ * @modified Paul Bonenfant Feb 2022
+ */
+public class Card {
+
+    public enum Suit {
+        HEARTS, CLUBS, SPADES, DIAMONDS
+    }
+
+    public enum Value {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+    }
+    
+    private final Suit suit;
+    private final Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public Value getValue() {
+        return this.value;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+}
